f1e59f86d769036541f076afc7c44e50be113e6b
Mahdi <mahdimeyghani02@gmail.com>
2024-08-04 19:23:52 +0330

    Create main window without widgets

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication
+import sys
+
+
 class MainWindow(QMainWindow):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Your Personal ChatBot")
+        self.setMinimumSize(700, 500)
+
+        self.show()
+
 
 class ChatBot:
     pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    sys.exit(app.exec())
