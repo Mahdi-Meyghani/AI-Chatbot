79414fea293ad121e3c3ec7d905f98059673fa0b
Mahdi <mahdimeyghani02@gmail.com>
2024-08-04 19:33:03 +0330

    Put the widgets in the window

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ class MainWindow(QMainWindow):
 
         # Add Chat Area Widget
         self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10, 10, 480, 320)
 
         # Add Input Area Widget
         self.input_area = QLineEdit(self)
+        self.input_area.setGeometry(10, 340, 480, 40)
 
         # Add Send Button Widget
         self.send_button = QPushButton("Send", self)
+        self.send_button.setGeometry(500, 340, 100, 40)
 
         self.show()
 
